fc571b8539daa7d9dffeb1f7dce33e2c36b56d23
mojoro <45500240+mojoro@users.noreply.github.com>
2024-03-08 22:46:22 +0100

    Finish functional logic minus extra credit points

--- scripts.js
+++ scripts.js
@@ -11,26 +11,77 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if(b === 0) return "Nice try"
     return a / b;
 }
-
-let firstNum;
-let secondNum;
-let operator;
 
 function operate(firstNum, secondNum, operator) {
     switch (operator){
         case '+':
-            add(firstNum, secondNum);
-            break;
+            return add(firstNum, secondNum);
+            
         case '-':
-            subtract(firstNum, secondNum);
-            break;
+            return subtract(firstNum, secondNum);
+
         case '*':
-            multiply(firstNum, secondNum);
-            break;
+            return multiply(firstNum, secondNum);
+
         case '/':
-            divide(firstNum, secondNum);
-            break;
+            return divide(firstNum, secondNum);
     }
+}
+
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let result = '';
+
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+
+numbers.forEach((button) => button.addEventListener('click', (e) => numberHandler(e)));
+operators.forEach((button) => button.addEventListener('click', (e) => operatorHandler(e)));
+equals.addEventListener('click', (e) => equalsHandler(e));
+
+clear.addEventListener('click', () => {
+    display.textContent = '';
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+})
+
+function numberHandler(e){
+    if (!operator){
+        firstNum += e.target.textContent;
+        display.textContent = firstNum;
+    }
+    else {
+        secondNum += e.target.textContent;
+        display.textContent = secondNum;
+    }
+}
+
+function operatorHandler(e){
+    if (!operator){
+        operator = e.target.textContent;
+    }
+    else{
+        result = operate(Number.parseInt(firstNum), Number.parseInt(secondNum), operator);
+        display.textContent = result;
+        operator = e.target.textContent;
+        firstNum = result;
+        secondNum = '';
+    }
+    console.log(e.target.textContent);
+}
+
+function equalsHandler(e) {
+    result = operate(Number.parseInt(firstNum), Number.parseInt(secondNum), operator);
+    display.textContent = result;
+    operator = '';
+    firstNum = result;
+    secondNum = '';
 }
